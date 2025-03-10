a7295aefcbd54e9a1a0cb824dad6a5095dec9408
BhavdipLakum <bhavdip.info@gmail.com>
2025-03-10 18:31:26 +0530

    database change

--- db.js
+++ db.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 
+// Add this line to address the deprecation warning
+mongoose.set("strictQuery", false);
+
 const dbUrl = process.env.MONGO_URI;
-console.log("Connecting to MongoDB:", dbUrl); 
+console.log("Connecting to MongoDB:", dbUrl);
 
 const connectToMongo = async () => {
   try {
